cac5f61500c9f2b84506d2d2fea17674ea16845e
Amit kohar <amitkohar@amits-air.home>
2023-02-10 11:14:13 -0500

    files with basic styles

--- controllers/employers.js
+++ controllers/employers.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+
 /** Get employers index(the module home page)  */
 router.get('/', (req, res) => {
-    res.render('employers/index', { title: 'Employers' });
+    //get data from json file
+    fs.readFile('./data/employers.json', 'utf8', (err, employers) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('employers/index', {
+                title: 'Employers',
+                employers: employers
+            });
+
+        }
+    });
+
 })
 
 module.exports = router;
